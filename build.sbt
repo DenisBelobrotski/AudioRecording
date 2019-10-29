a9d2a776c453108bd91acfbe4b7694fc098bd505
Denis Belobrotski <belobrotskiy@gmail.com>
2019-10-29 21:53:01 +0300

    Hello world

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "AudioRecording"
+
+version := "0.1"
+
+scalaVersion := "2.13.1"
---
+++ src/main/scala/AudioRecording.scala
@@ -0,0 +1,5 @@
+object AudioRecording {
+    def main(args: Array[String]): Unit = {
+        println("Hello, world!")
+    }
+}
